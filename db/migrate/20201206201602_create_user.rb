d71e7258896f5556566aa42e10cc307889790746
David <dav2070@web.de>
2020-12-06 22:22:14 +0100

    Add User table and model

---
+++ db/migrate/20201206201602_create_user.rb
@@ -0,0 +1,22 @@
+class CreateUser < ActiveRecord::Migration[6.0]
+  def change
+	 create_table :users do |t|
+		t.string :email
+		t.string :first_name
+		t.string :password
+		t.boolean :confirmed, default: false
+		t.string :email_confirmation_token
+		t.string :password_confirmation_token
+		t.string :old_email
+		t.string :new_email
+		t.string :new_password
+		t.bigint :used_storage, default: 0
+		t.datetime :last_active
+		t.string :stripe_customer_id
+		t.integer :plan, default: 0
+		t.integer :subscription_status, default: 0
+		t.timestamp :period_end
+		t.timestamps
+    end
+  end
+end
