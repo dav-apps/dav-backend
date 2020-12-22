c82962792dbabdefe33de7ad4e924f3399696f3c
David <dav2070@web.de>
2020-12-23 00:24:05 +0100

    Add tests for get_table_object endpoint
    - Add more test data

--- test/controllers/table_objects_controller_test.rb
+++ test/controllers/table_objects_controller_test.rb
@@ -595,4 +595,211 @@ describe TableObjectsController do
 		app_user = app_users(:mattCards)
 		assert(Time.now.to_i - app_user.last_active.to_i < 10)
 	end
+
+	# get_table_object
+	it "should not get table object without jwt" do
+		res = get_request(
+			"/v1/table_object/1"
+		)
+
+		assert_response 401
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_MISSING, res["errors"][0]["code"])
+	end
+
+	it "should not get table object with invalid jwt" do
+		res = get_request(
+			"/v1/table_object/1",
+			{Authorization: "asdasdasd"}
+		)
+
+		assert_response 404
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::SESSION_DOES_NOT_EXIST, res["errors"][0]["code"])
+	end
+
+	it "should not get table object that does not exist" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+
+		res = get_request(
+			"/v1/table_object/-123",
+			{Authorization: jwt}
+		)
+
+		assert_response 404
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::TABLE_OBJECT_DOES_NOT_EXIST, res["errors"][0]["code"])
+	end
+
+	it "should not get table object that belongs to another user" do
+		jwt = generate_jwt(sessions(:davCardsSession))
+
+		res = get_request(
+			"/v1/table_object/#{table_objects(:mattSecondCard).id}",
+			{Authorization: jwt}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should not get table object with session that does not belong to the app" do
+		jwt = generate_jwt(sessions(:davWebsiteSession))
+
+		res = get_request(
+			"/v1/table_object/#{table_objects(:davFirstCard).id}",
+			{Authorization: jwt}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should get table object" do
+		jwt = generate_jwt(sessions(:sherlockTestAppSession))
+		table_object = table_objects(:sherlockTestData)
+
+		res = get_request(
+			"/v1/table_object/#{table_object.id}",
+			{Authorization: jwt}
+		)
+
+		assert_response 200
+
+		assert_equal(table_object.id, res["id"])
+		assert_equal(table_object.user_id, res["user_id"])
+		assert_equal(table_object.table_id, res["table_id"])
+		assert_equal(table_object.uuid, res["uuid"])
+		assert_equal(table_object.file, res["file"])
+		assert_equal(generate_table_object_etag(table_object), res["etag"])
+		assert_equal(4, res["properties"].length)
+
+		first_property = table_object.table_object_properties.where(name: "test1").first
+		assert_equal(first_property.value, res["properties"][first_property.name])
+		assert(res["properties"][first_property.name].is_a?(String))
+
+		second_property = table_object.table_object_properties.where(name: "test2").first
+		assert_equal(second_property.value == "true", res["properties"][second_property.name])
+		assert(res["properties"][second_property.name].is_a?(TrueClass))
+
+		third_property = table_object.table_object_properties.where(name: "test3").first
+		assert_equal(third_property.value.to_i, res["properties"][third_property.name])
+		assert(res["properties"][third_property.name].is_a?(Integer))
+
+		fourth_property = table_object.table_object_properties.where(name: "test4").first
+		assert_equal(fourth_property.value.to_f, res["properties"][fourth_property.name])
+		assert(res["properties"][fourth_property.name].is_a?(Float))
+	end
+
+	it "should get table object with uuid" do
+		jwt = generate_jwt(sessions(:sherlockTestAppSession))
+		table_object = table_objects(:sherlockTestData)
+
+		res = get_request(
+			"/v1/table_object/#{table_object.uuid}",
+			{Authorization: jwt}
+		)
+
+		assert_response 200
+
+		assert_equal(table_object.id, res["id"])
+		assert_equal(table_object.user_id, res["user_id"])
+		assert_equal(table_object.table_id, res["table_id"])
+		assert_equal(table_object.uuid, res["uuid"])
+		assert_equal(table_object.file, res["file"])
+		assert_equal(generate_table_object_etag(table_object), res["etag"])
+		assert_equal(4, res["properties"].length)
+
+		first_property = table_object.table_object_properties.where(name: "test1").first
+		assert_equal(first_property.value, res["properties"][first_property.name])
+		assert(res["properties"][first_property.name].is_a?(String))
+
+		second_property = table_object.table_object_properties.where(name: "test2").first
+		assert_equal(second_property.value == "true", res["properties"][second_property.name])
+		assert(res["properties"][second_property.name].is_a?(TrueClass))
+
+		third_property = table_object.table_object_properties.where(name: "test3").first
+		assert_equal(third_property.value.to_i, res["properties"][third_property.name])
+		assert(res["properties"][third_property.name].is_a?(Integer))
+
+		fourth_property = table_object.table_object_properties.where(name: "test4").first
+		assert_equal(fourth_property.value.to_f, res["properties"][fourth_property.name])
+		assert(res["properties"][fourth_property.name].is_a?(Float))
+	end
+
+	it "should get table object with access" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+		table_object = table_objects(:davFirstCard)
+
+		res = get_request(
+			"/v1/table_object/#{table_object.id}",
+			{Authorization: jwt}
+		)
+
+		assert_response 200
+
+		assert_equal(table_object.id, res["id"])
+		assert_equal(table_object.user_id, res["user_id"])
+		assert_equal(table_object.table_id, res["table_id"])
+		assert_equal(table_object.uuid, res["uuid"])
+		assert_equal(table_object.file, res["file"])
+		assert_equal(generate_table_object_etag(table_object), res["etag"])
+		assert_equal(2, res["properties"].length)
+
+		first_property = table_object.table_object_properties.where(name: "page1").first
+		assert_equal(first_property.value, res["properties"][first_property.name])
+		assert(res["properties"][first_property.name].is_a?(String))
+
+		second_property = table_object.table_object_properties.where(name: "page2").first
+		assert_equal(second_property.value, res["properties"][second_property.name])
+		assert(res["properties"][second_property.name].is_a?(String))
+	end
+
+	it "should get table with uuid with access" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+		table_object = table_objects(:davFirstCard)
+
+		res = get_request(
+			"/v1/table_object/#{table_object.uuid}",
+			{Authorization: jwt}
+		)
+
+		assert_response 200
+
+		assert_equal(table_object.id, res["id"])
+		assert_equal(table_object.user_id, res["user_id"])
+		assert_equal(table_object.table_id, res["table_id"])
+		assert_equal(table_object.uuid, res["uuid"])
+		assert_equal(table_object.file, res["file"])
+		assert_equal(generate_table_object_etag(table_object), res["etag"])
+		assert_equal(2, res["properties"].length)
+
+		first_property = table_object.table_object_properties.where(name: "page1").first
+		assert_equal(first_property.value, res["properties"][first_property.name])
+		assert(res["properties"][first_property.name].is_a?(String))
+
+		second_property = table_object.table_object_properties.where(name: "page2").first
+		assert_equal(second_property.value, res["properties"][second_property.name])
+		assert(res["properties"][second_property.name].is_a?(String))
+	end
+
+	it "should get table object and update last_active fields" do
+		jwt = generate_jwt(sessions(:sherlockTestAppSession))
+		table_object = table_objects(:sherlockTestData)
+
+		res = get_request(
+			"/v1/table_object/#{table_object.id}",
+			{Authorization: jwt}
+		)
+
+		assert_response 200
+
+		user = users(:sherlock)
+		assert(Time.now.to_i - user.last_active.to_i < 10)
+
+		app_user = app_users(:sherlockTestApp)
+		assert(Time.now.to_i - app_user.last_active.to_i < 10)
+	end
 end
