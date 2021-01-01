79d60476ebd1e6663432a26ab9b740d0b5e364b5
David <dav2070@web.de>
2021-01-01 16:45:08 +0100

    Add tests for update_notification endpoint

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -244,4 +244,188 @@ describe NotificationsController do
 		assert_equal(notification.title, res["title"])
 		assert_equal(notification.body, res["body"])
 	end
+
+	# update_notification
+	it "should not update notification without jwt" do
+		res = put_request("/v1/notification/23234234")
+
+		assert_response 401
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_MISSING, res["errors"][0]["code"])
+	end
+
+	it "should not update notification without Content-Type json" do
+		res = put_request(
+			"/v1/notification/20234j23",
+			{Authorization: "asdasdasdasd"}
+		)
+
+		assert_response 415
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::CONTENT_TYPE_NOT_SUPPORTED, res["errors"][0]["code"])
+	end
+
+	it "should not update notification with invalid jwt" do
+		res = put_request(
+			"/v1/notification/asdasdasd",
+			{Authorization: "asdasdasd", 'Content-Type': 'application/json'}
+		)
+
+		assert_response 401
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_INVALID, res["errors"][0]["code"])
+	end
+
+	it "should not update notification with properties with wrong types" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+		notification = notifications(:mattCardsFirstReminderNotification)
+
+		res = put_request(
+			"/v1/notification/#{notification.uuid}",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				time: true,
+				interval: "Hello World",
+				title: 123.4,
+				body: false
+			}
+		)
+
+		assert_response 400
+		assert_equal(4, res["errors"].length)
+		assert_equal(ErrorCodes::TIME_WRONG_TYPE, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::INTERVAL_WRONG_TYPE, res["errors"][1]["code"])
+		assert_equal(ErrorCodes::TITLE_WRONG_TYPE, res["errors"][2]["code"])
+		assert_equal(ErrorCodes::BODY_WRONG_TYPE, res["errors"][3]["code"])
+	end
+
+	it "should not update notification with too short properties" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+		notification = notifications(:mattCardsFirstReminderNotification)
+
+		res = put_request(
+			"/v1/notification/#{notification.uuid}",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				title: "a",
+				body: "a"
+			}
+		)
+
+		assert_response 400
+		assert_equal(2, res["errors"].length)
+		assert_equal(ErrorCodes::TITLE_TOO_SHORT, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::BODY_TOO_SHORT, res["errors"][1]["code"])
+	end
+
+	it "should not update notification with too long properties" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+		notification = notifications(:mattCardsFirstReminderNotification)
+
+		res = put_request(
+			"/v1/notification/#{notification.uuid}",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				title: "a" * 200,
+				body: "a" * 200
+			}
+		)
+
+		assert_response 400
+		assert_equal(2, res["errors"].length)
+		assert_equal(ErrorCodes::TITLE_TOO_LONG, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::BODY_TOO_LONG, res["errors"][1]["code"])
+	end
+
+	it "should not update notification that does not exist" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+
+		res = put_request(
+			"/v1/notification/ioahsd9h83q9dasd",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				interval: 0
+			}
+		)
+
+		assert_response 404
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::NOTIFICATION_DOES_NOT_EXIST, res["errors"][0]["code"])
+	end
+
+	it "should not update notification that does not belong to the user" do
+		jwt = generate_jwt(sessions(:davCardsSession))
+		notification = notifications(:mattCardsFirstReminderNotification)
+
+		res = put_request(
+			"/v1/notification/#{notification.uuid}",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				interval: 0
+			}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should not update notification that does not belong to the app" do
+		jwt = generate_jwt(sessions(:mattWebsiteSession))
+		notification = notifications(:mattCardsFirstReminderNotification)
+
+		res = put_request(
+			"/v1/notification/#{notification.uuid}",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				interval: 0
+			}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should update notification" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+		notification = notifications(:mattCardsFirstReminderNotification)
+		time = Time.now.to_i
+		interval = 13123123
+		title = "Updated title"
+		body = "Updated body"
+
+		res = put_request(
+			"/v1/notification/#{notification.uuid}",
+			{Authorization: jwt, 'Content-Type': 'application/json'},
+			{
+				time: time,
+				interval: interval,
+				title: title,
+				body: body
+			}
+		)
+
+		assert_response 200
+		
+		assert_equal(notification.id, res["id"])
+		assert_equal(notification.user_id, res["user_id"])
+		assert_equal(notification.app_id, res["app_id"])
+		assert_equal(notification.uuid, res["uuid"])
+		assert_equal(time, res["time"])
+		assert_equal(interval, res["interval"])
+		assert_equal(title, res["title"])
+		assert_equal(body, res["body"])
+
+		notification = Notification.find_by(id: notification.id)
+		assert_not_nil(notification)
+		assert_equal(notification.id, res["id"])
+		assert_equal(notification.user_id, res["user_id"])
+		assert_equal(notification.app_id, res["app_id"])
+		assert_equal(notification.uuid, res["uuid"])
+		assert_equal(notification.time.to_i, res["time"])
+		assert_equal(notification.interval, res["interval"])
+		assert_equal(notification.title, res["title"])
+		assert_equal(notification.body, res["body"])
+	end
 end
