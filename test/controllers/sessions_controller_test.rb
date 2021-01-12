c9692aa8946c00e32139da7e229b7d6de136a94f
David <dav2070@web.de>
2021-01-12 21:15:08 +0100

    Add tests for create_session_from_jwt endpoint

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -25,7 +25,7 @@ describe SessionsController do
 		assert_equal(ErrorCodes::CONTENT_TYPE_NOT_SUPPORTED, res["errors"][0]["code"])
 	end
 
-	it "should not create session without requred properties" do
+	it "should not create session without required properties" do
 		res = post_request(
 			"/v1/session",
 			{Authorization: "asdasd", 'Content-Type': 'application/json'}
@@ -379,6 +379,329 @@ describe SessionsController do
 				password: Constants::MATT_PASSWORD,
 				app_id: app.id,
 				api_key: devs(:dav).api_key,
+				device_name: device_name,
+				device_type: device_type,
+				device_os: device_os
+			}
+		)
+
+		assert_response 201
+		assert_not_nil(res["jwt"])
+
+		# Check the session
+		session_id = res["jwt"].split('.').last.to_i
+		assert_not_equal(0, session_id)
+		session = Session.find_by(id: session_id)
+		assert_not_nil(session)
+		assert_equal(session_id, session.id)
+		assert_equal(user, session.user)
+		assert_equal(app, session.app)
+		assert_equal(device_name, session.device_name)
+		assert_equal(device_type, session.device_type)
+		assert_equal(device_os, session.device_os)
+	end
+
+	# create_session_from_jwt
+	it "should not create session from jwt without auth" do
+		res = post_request("/v1/session/jwt")
+
+		assert_response 401
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::AUTH_HEADER_MISSING, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt without Content-Type jwt" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "asdasdasdasd"}
+		)
+
+		assert_response 415
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::CONTENT_TYPE_NOT_SUPPORTED, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt without required properties" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "asdasasd", 'Content-Type': 'application/json'}
+		)
+
+		assert_response 400
+		assert_equal(3, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_MISSING, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::APP_ID_MISSING, res["errors"][1]["code"])
+		assert_equal(ErrorCodes::API_KEY_MISSING, res["errors"][2]["code"])
+	end
+
+	it "should not create session from jwt with properties with wrong types" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "asdasdasdas", 'Content-Type': 'application/json'},
+			{
+				jwt: 124.5,
+				app_id: true,
+				api_key: 642
+			}
+		)
+
+		assert_response 400
+		assert_equal(3, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_WRONG_TYPE, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::APP_ID_WRONG_TYPE, res["errors"][1]["code"])
+		assert_equal(ErrorCodes::API_KEY_WRONG_TYPE, res["errors"][2]["code"])
+	end
+
+	it "should not create session from jwt with optional properties with wrong types" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "asdasdasdas", 'Content-Type': 'application/json'},
+			{
+				jwt: 124.5,
+				app_id: true,
+				api_key: 642,
+				device_name: false,
+				device_type: 963.2,
+				device_os: 24
+			}
+		)
+
+		assert_response 400
+		assert_equal(6, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_WRONG_TYPE, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::APP_ID_WRONG_TYPE, res["errors"][1]["code"])
+		assert_equal(ErrorCodes::API_KEY_WRONG_TYPE, res["errors"][2]["code"])
+		assert_equal(ErrorCodes::DEVICE_NAME_WRONG_TYPE, res["errors"][3]["code"])
+		assert_equal(ErrorCodes::DEVICE_TYPE_WRONG_TYPE, res["errors"][4]["code"])
+		assert_equal(ErrorCodes::DEVICE_OS_WRONG_TYPE, res["errors"][5]["code"])
+	end
+
+	it "should not create session from jwt with too short optional properties" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "adasdasdasd", 'Content-Type': 'application/json'},
+			{
+				jwt: "spjdjfsodfsdfi",
+				app_id: 1,
+				api_key: "sodfsjdgsdnjksfdnklfdfd",
+				device_name: "a",
+				device_type: "a",
+				device_os: "a"
+			}
+		)
+
+		assert_response 400
+		assert_equal(3, res["errors"].length)
+		assert_equal(ErrorCodes::DEVICE_NAME_TOO_SHORT, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::DEVICE_TYPE_TOO_SHORT, res["errors"][1]["code"])
+		assert_equal(ErrorCodes::DEVICE_OS_TOO_SHORT, res["errors"][2]["code"])
+	end
+
+	it "should not create session from jwt with too long optional properties" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "adasdasdasd", 'Content-Type': 'application/json'},
+			{
+				jwt: "spjdjfsodfsdfi",
+				app_id: 1,
+				api_key: "sodfsjdgsdnjksfdnklfdfd",
+				device_name: "a" * 50,
+				device_type: "a" * 50,
+				device_os: "a" * 50
+			}
+		)
+
+		assert_response 400
+		assert_equal(3, res["errors"].length)
+		assert_equal(ErrorCodes::DEVICE_NAME_TOO_LONG, res["errors"][0]["code"])
+		assert_equal(ErrorCodes::DEVICE_TYPE_TOO_LONG, res["errors"][1]["code"])
+		assert_equal(ErrorCodes::DEVICE_OS_TOO_LONG, res["errors"][2]["code"])
+	end
+
+	it "should not create session from jwt with dev that does not exist" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "asdasdasd,asdasdasdasd", 'Content-Type': 'application/json'},
+			{
+				jwt: "spjdjfsodfsdfi",
+				app_id: 1,
+				api_key: "sodfsjdgsdnjksfdnklfdfd"
+			}
+		)
+
+		assert_response 404
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::DEV_DOES_NOT_EXIST, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt with invalid auth" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: "v05Bmn5pJT_pZu6plPQQf8qs4ahnK3cv2tkEK5XJ,13wdfio23r8hifwe", 'Content-Type': 'application/json'},
+			{
+				jwt: "asdasdasdasd",
+				app_id: 1,
+				api_key: "asdassda"
+			}
+		)
+
+		assert_response 401
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::AUTHENTICATION_FAILED, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt with another dev than the first one" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(devs(:dav)), 'Content-Type': 'application/json'},
+			{
+				jwt: "asdasdasdasd",
+				app_id: 1,
+				api_key: "asdassda"
+			}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt with invalid jwt" do
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(devs(:sherlock)), 'Content-Type': 'application/json'},
+			{
+				jwt: "asdasdasdasd",
+				app_id: 1,
+				api_key: "asdassda"
+			}
+		)
+
+		assert_response 401
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::JWT_INVALID, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt that is not for the website" do
+		jwt = generate_jwt(sessions(:mattCardsSession))
+
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(devs(:sherlock)), 'Content-Type': 'application/json'},
+			{
+				jwt: jwt,
+				app_id: 1,
+				api_key: "aasdasdasdad"
+			}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt for app that does not exist" do
+		jwt = generate_jwt(sessions(:mattWebsiteSession))
+
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(devs(:sherlock)), 'Content-Type': 'application/json'},
+			{
+				jwt: jwt,
+				app_id: -1233,
+				api_key: "sadadasdasdasd"
+			}
+		)
+
+		assert_response 404
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::APP_DOES_NOT_EXIST, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt with api key for dev that does not exist" do
+		jwt = generate_jwt(sessions(:mattWebsiteSession))
+
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(devs(:sherlock)), 'Content-Type': 'application/json'},
+			{
+				jwt: jwt,
+				app_id: apps(:cards).id,
+				api_key: "sadasdassda"
+			}
+		)
+
+		assert_response 404
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::DEV_DOES_NOT_EXIST, res["errors"][0]["code"])
+	end
+
+	it "should not create session from jwt for app that does not belong to the dev" do
+		jwt = generate_jwt(sessions(:mattWebsiteSession))
+
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(devs(:sherlock)), 'Content-Type': 'application/json'},
+			{
+				jwt: jwt,
+				app_id: apps(:cards).id,
+				api_key: devs(:dav).api_key
+			}
+		)
+
+		assert_response 403
+		assert_equal(1, res["errors"].length)
+		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
+	end
+
+	it "should create session from jwt" do
+		jwt = generate_jwt(sessions(:mattWebsiteSession))
+		dev = devs(:sherlock)
+		user = users(:matt)
+		app = apps(:cards)
+
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(dev), 'Content-Type': 'application/json'},
+			{
+				jwt: jwt,
+				app_id: app.id,
+				api_key: dev.api_key
+			}
+		)
+
+		assert_response 201
+		assert_not_nil(res["jwt"])
+
+		# Check the session
+		session_id = res["jwt"].split('.').last.to_i
+		assert_not_equal(0, session_id)
+		session = Session.find_by(id: session_id)
+		assert_not_nil(session)
+		assert_equal(session_id, session.id)
+		assert_equal(user, session.user)
+		assert_equal(app, session.app)
+		assert_nil(session.device_name)
+		assert_nil(session.device_type)
+		assert_nil(session.device_os)
+	end
+
+	it "should create session from jwt with device info" do
+		jwt = generate_jwt(sessions(:mattWebsiteSession))
+		dev = devs(:sherlock)
+		user = users(:matt)
+		app = apps(:cards)
+		device_name = "Surface Phone"
+		device_type = "Dual-Screen"
+		device_os = "Andromeda"
+
+		res = post_request(
+			"/v1/session/jwt",
+			{Authorization: generate_auth(dev), 'Content-Type': 'application/json'},
+			{
+				jwt: jwt,
+				app_id: app.id,
+				api_key: dev.api_key,
 				device_name: device_name,
 				device_type: device_type,
 				device_os: device_os
