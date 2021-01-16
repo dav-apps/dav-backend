843a69a35e6948be53954d7b4b6f7aefc0000922
David <dav2070@web.de>
2021-01-16 18:00:31 +0100

    Add tests for get_app_user_activities and add test data

--- test/controllers/app_user_activities_controller_test.rb
+++ test/controllers/app_user_activities_controller_test.rb
@@ -1,23 +1,23 @@
 require "test_helper"
 
-describe UserActivitiesController do
+describe AppUserActivitiesController do
 	setup do
 		setup
 	end
 
-	# get_user_activities
-	it "should not get user activities without jwt" do
-		res = get_request("/v1/user_activities")
+	# get_app_user_activities
+	it "should not get app user activities without jwt" do
+		res = get_request("/v1/app/1/user_activities")
 
 		assert_response 401
 		assert_equal(1, res["errors"].length)
 		assert_equal(ErrorCodes::AUTH_HEADER_MISSING, res["errors"][0]["code"])
 	end
 
-	it "should not get user activities with invalid jwt" do
+	it "should not get app user activities with invalid jwt" do
 		res = get_request(
-			"/v1/user_activities",
-			{Authorization: "asdasdasdasdads"}
+			"/v1/app/1/user_activities",
+			{Authorization: "asdasdasdasd"}
 		)
 
 		assert_response 401
@@ -25,11 +25,11 @@ describe UserActivitiesController do
 		assert_equal(ErrorCodes::JWT_INVALID, res["errors"][0]["code"])
 	end
 
-	it "should not get user activities from another app than the website" do
+	it "should not get app user activities from another app than the website" do
 		jwt = generate_jwt(sessions(:sherlockTestAppSession))
 
 		res = get_request(
-			"/v1/user_activities",
+			"/v1/app/1/user_activities",
 			{Authorization: jwt}
 		)
 
@@ -38,11 +38,11 @@ describe UserActivitiesController do
 		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
 	end
 
-	it "should not get user activities with another dev than the first one" do
-		jwt = generate_jwt(sessions(:davWebsiteSession))
+	it "should not get app user activities for the app of another dev" do
+		jwt = generate_jwt(sessions(:sherlockWebsiteSession))
 
 		res = get_request(
-			"/v1/user_activities",
+			"/v1/app/#{apps(:pocketlib).id}/user_activities",
 			{Authorization: jwt}
 		)
 
@@ -51,15 +51,18 @@ describe UserActivitiesController do
 		assert_equal(ErrorCodes::ACTION_NOT_ALLOWED, res["errors"][0]["code"])
 	end
 
-	it "should get user activities" do
+	it "should get app user activities" do
 		jwt = generate_jwt(sessions(:sherlockWebsiteSession))
-		first_user_activity = UserActivity.create(
+		app = apps(:cards)
+		first_user_activity = AppUserActivity.create(
+			app: app,
 			time: (Time.now - 3.days).beginning_of_day,
 			count_daily: 3,
 			count_monthly: 9,
 			count_yearly: 20
 		)
-		second_user_activity = UserActivity.create(
+		second_user_activity = AppUserActivity.create(
+			app: app,
 			time: (Time.now - 20.days).beginning_of_day,
 			count_daily: 4,
 			count_monthly: 7,
@@ -67,7 +70,7 @@ describe UserActivitiesController do
 		)
 
 		res = get_request(
-			"/v1/user_activities",
+			"/v1/app/#{app.id}/user_activities",
 			{Authorization: jwt}
 		)
 
@@ -85,15 +88,16 @@ describe UserActivitiesController do
 		assert_equal(second_user_activity.count_yearly, res["days"][1]["count_yearly"])
 	end
 
-	it "should get user activities in the specified timeframe" do
+	it "should get app user activities in the specified timeframe" do
 		jwt = generate_jwt(sessions(:sherlockWebsiteSession))
+		app = apps(:cards)
 		start_timestamp = DateTime.parse("2019-06-09T00:00:00.000Z").to_i
 		end_timestamp = DateTime.parse("2019-06-12T00:00:00.000Z").to_i
-		first_user_activity = user_activities(:first_user_activity)
-		second_user_activity = user_activities(:second_user_activity)
+		first_user_activity = app_user_activities(:first_cards_user_activity)
+		second_user_activity = app_user_activities(:second_cards_user_activity)
 
 		res = get_request(
-			"/v1/user_activities?start=#{start_timestamp}&end=#{end_timestamp}",
+			"/v1/app/#{app.id}/user_activities?start=#{start_timestamp}&end=#{end_timestamp}",
 			{Authorization: jwt}
 		)
 
