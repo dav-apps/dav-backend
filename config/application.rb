41b212659a4400a574e87e94bb194fb059b514b2
David <dav2070@web.de>
2021-03-05 22:54:24 +0100

    Add allowed request origins

--- config/application.rb
+++ config/application.rb
@@ -20,18 +20,55 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 module DavBackend
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+  	class Application < Rails::Application
+		# Initialize configuration defaults for originally generated Rails version.
+		config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+		# Settings in config/environments/* take precedence over those specified here.
+		# Application configuration can go into files in config/initializers
+		# -- all .rb files in that directory are automatically loaded after loading
+		# the framework and any gems in your application.
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-  end
+		# Only loads a smaller set of middleware suitable for API only apps.
+		# Middleware like session, flash, cookies can be added back manually.
+		# Skip views, helpers and assets when generating a new resource.
+		config.api_only = true
+	 
+		config.action_cable.allowed_request_origins = [
+			nil,
+			"http://localhost:3000",
+			"http://localhost:3001",
+			"http://localhost:3002",
+			"https://dav-apps.tech",
+			"https://dav-website.azurewebsites.net",
+			"https://dav-website-staging.azurewebsites.net",
+			"https://calendo.dav-apps.tech",
+			"https://calendo-dav.azurewebsites.net",
+      	"https://calendo-dav-staging.azurewebsites.net",
+			"https://pocketlib.dav-apps.tech",
+    		"https://pocketlib-dav.azurewebsites.net",
+    		"https://pocketlib-dav-staging.azurewebsites.net"
+		]
+
+		Rails.application.config.middleware.insert_before 0, Rack::Cors do
+			allow do
+				origins 'localhost:3000',
+					'localhost:3001',
+					'localhost:3002',
+					'dav-apps.tech',
+					'dav-website.azurewebsites.net',
+					'dav-website-staging.azurewebsites.net',
+					'calendo.dav-apps.tech',
+					'calendo-dav.azurewebsites.net',
+					'calendo-dav-staging.azurewebsites.net',
+					'pocketlib.dav-apps.tech',
+					'pocketlib-dav.azurewebsites.net',
+					'pocketlib-dav-staging.azurewebsites.net'
+
+				resource '*',
+				headers: :any,
+          	methods: %i(get post put patch delete options head)
+			end
+		end
+  	end
 end
