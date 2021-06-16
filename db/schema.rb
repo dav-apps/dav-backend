0cb9064838b4661cec87e25980b52c1e30e26e23
David <dav2070@web.de>
2021-06-16 17:02:07 +0200

    Add migration generated by rails update command

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_174049) do
+ActiveRecord::Schema.define(version: 2021_06_15_172944) do
 
   create_table "api_endpoint_request_cache_params", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "api_endpoint_request_cache_id"
