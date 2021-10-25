2c1ee4fe0a8c4b6ff4d9adccd31c7e90d61b6281
David <dav2070@web.de>
2021-10-25 21:12:44 +0200

    Update update_api_caches to execute only outdated caches
    - Go through all TableObjectChanges and find all corresponding ApiEndpointRequestCacheDependencies to set the parent ApiEndpointRequestCaches to old
    - Reload only the old caches and update the dependencies in the database

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,36 @@ class TasksController < ApplicationController
 	end
 
 	def update_api_caches
+		# Go through all changes in the database
+		TableObjectChange.all.each do |obj_change|
+			# Find the corresponding ApiEndpointRequestCacheDependencies
+			# Look for each combination of ids
+			ApiEndpointRequestCacheDependency
+				.where("user_id = ? OR table_id = ? OR table_object_id = ? OR collection_id = ?", obj_change.user_id, obj_change.table_id, obj_change.table_object_id, obj_change.collection_id)
+				.each { |dependency| set_api_endpoint_request_cache_as_old(dependency.api_endpoint_request_cache) }
+
+			if !obj_change.table_object.nil?
+				# Look for the user the table object belongs to
+				ApiEndpointRequestCacheDependency.where(user: obj_change.table_object.user).each do |dependency|
+					set_api_endpoint_request_cache_as_old(dependency.api_endpoint_request_cache)
+				end
+
+				# Look for each table the table object belongs to
+				ApiEndpointRequestCacheDependency.where(table: obj_change.table_object.table).each do |dependency|
+					set_api_endpoint_request_cache_as_old(dependency.api_endpoint_request_cache)
+				end
+
+				# Look for each collection the table object belongs to
+				obj_change.table_object.collections.each do |collection|
+					ApiEndpointRequestCacheDependency.where(collection: collection).each do |dependency|
+						set_api_endpoint_request_cache_as_old(dependency.api_endpoint_request_cache)
+					end
+				end
+			end
+
+			obj_change.destroy!
+		end
+
 		Api.all.each do |api|
 			if ENV["USE_COMPILED_API_ENDPOINTS"] == "true" && !Rails.env.test?
 				# Get the prod slot
@@ -22,7 +52,7 @@ class TasksController < ApplicationController
 					# Get the compiled endpoint
 					compiled_endpoint = api_endpoint.compiled_api_endpoints.find_by(api_slot: prod_slot)
 
-					api_endpoint.api_endpoint_request_caches.each do |cache|
+					api_endpoint.api_endpoint_request_caches.where(old: true).each do |cache|
 						# Get the params
 						url_params = Hash.new
 						cache.api_endpoint_request_cache_params.each do |param|
@@ -42,7 +72,26 @@ class TasksController < ApplicationController
 						if result[:status] == 200 && !result[:file]
 							# Update the cache
 							cache.response = result[:data].to_json
+							cache.old = false
 							cache.save
+						end
+
+						# Save the new dependencies in the database
+						old_dependencies = cache.api_endpoint_request_cache_dependencies
+						new_dependencies = result[:dependencies]
+
+						# Remove all old dependencies and save all new dependencies
+						old_dependencies.each { |dependency| dependency.destroy! }
+
+						new_dependencies.each do |dependency|
+							ApiEndpointRequestCacheDependency.create(
+								user_id: dependency[:user_id],
+								table_id: dependency[:table_id],
+								table_object_id: dependency[:table_object_id],
+								collection_id: dependency[:collection_id],
+								api_endpoint_request_cache: cache,
+								name: dependency[:name]
+							)
 						end
 					end
 				end
@@ -56,7 +105,7 @@ class TasksController < ApplicationController
 						env_vars[env_var.name] = UtilsService.convert_env_value(env_var.class_name, env_var.value)
 					end
 
-					api_endpoint.api_endpoint_request_caches.each do |cache|
+					api_endpoint.api_endpoint_request_caches.where(old: true).each do |cache|
 						vars = Hash.new
 						url_params = Hash.new
 						vars["env"] = env_vars
@@ -80,7 +129,26 @@ class TasksController < ApplicationController
 						if result[:status] == 200 && !result[:file]
 							# Update the cache
 							cache.response = result[:data].to_json
+							cache.old = false
 							cache.save
+						end
+
+						# Save the new dependencies in the database
+						old_dependencies = cache.api_endpoint_request_cache_dependencies
+						new_dependencies = result[:dependencies]
+
+						# Remove all old dependencies and save all new dependencies
+						old_dependencies.each { |dependency| dependency.destroy! }
+
+						new_dependencies.each do |dependency|
+							ApiEndpointRequestCacheDependency.create(
+								user_id: dependency[:user_id],
+								table_id: dependency[:table_id],
+								table_object_id: dependency[:table_object_id],
+								collection_id: dependency[:collection_id],
+								api_endpoint_request_cache: cache,
+								name: dependency[:name]
+							)
 						end
 					end
 				end
@@ -189,5 +257,12 @@ class TasksController < ApplicationController
 
 	def user_was_active(last_active, timeframe)
 		last_active.nil? ? false : Time.now - last_active < timeframe
+	end
+
+	def set_api_endpoint_request_cache_as_old(cache)
+		return if cache.old
+
+		cache.old = true
+		cache.save
 	end
 end
