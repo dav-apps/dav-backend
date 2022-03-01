a78854717493b34481699ad1bd2469088e016f71
David <dav2070@web.de>
2022-03-01 16:54:19 +0100

    Update DavExpressionCompiler to use db 1 of redis

--- app/classes/dav_expression_compiler.rb
+++ app/classes/dav_expression_compiler.rb
@@ -1049,7 +1049,7 @@ class DavExpressionCompiler
 		@vars[:dependencies] = Array.new
 		@vars[:apps] = Hash.new
 		@vars[:tables] = Hash.new
-		@vars[:redis] = Redis.new(url: ENV["REDIS_URL"])
+		@vars[:redis] = Redis.new(url: ENV["REDIS_URL"], db: 1)
 
 		eval props[:code]
 	end
