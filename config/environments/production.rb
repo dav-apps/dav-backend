872a2819222d0d0a94fa07edcba9eba9e16dc7b1
David <zurek.dav@gmail.com>
2023-01-26 20:39:44 +0100

    Add resend config to prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.resend_settings = {
+    api_key: ENV['RESEND_API_KEY'],
+  }
 end
