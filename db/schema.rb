3226b853304daca7bf5546ff85c6cbfcc2b22b92
David <zurek.dav@gmail.com>
2023-10-15 01:45:14 +0200

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
   create_table "api_endpoints", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "api_slot_id"
     t.string "path"
     t.string "method"
     t.text "commands"
     t.boolean "caching", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "api_env_vars", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -40,27 +39,27 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "name"
     t.string "params"
     t.text "commands"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "api_slots", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "api_id"
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "apis", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "app_id"
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "app_user_snapshots", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "app_id"
-    t.datetime "time"
+    t.datetime "time", precision: nil
     t.integer "daily_active", default: 0
     t.integer "monthly_active", default: 0
     t.integer "yearly_active", default: 0
@@ -76,9 +75,9 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.bigint "user_id"
     t.bigint "app_id"
     t.bigint "used_storage", default: 0
-    t.datetime "last_active"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "last_active", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "apps", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -89,22 +88,22 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "web_link"
     t.string "google_play_link"
     t.string "microsoft_store_link"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "collections", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "table_id"
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "compiled_api_endpoints", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "api_endpoint_id"
     t.text "code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "devs", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -112,28 +111,28 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "api_key"
     t.string "secret_key"
     t.string "uuid"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notifications", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "app_id"
     t.string "uuid"
-    t.datetime "time"
+    t.datetime "time", precision: nil
     t.integer "interval"
     t.string "title"
     t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["uuid"], name: "index_notifications_on_uuid", unique: true
   end
 
   create_table "providers", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.string "stripe_account_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "purchases", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -146,8 +145,8 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.integer "price"
     t.string "currency"
     t.boolean "completed", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "uuid"
     t.index ["uuid"], name: "index_purchases_on_uuid", unique: true
   end
@@ -155,8 +154,8 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
   create_table "redis_table_object_operations", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "table_object_uuid"
     t.string "operation"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -166,8 +165,8 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "old_token"
     t.string "device_name"
     t.string "device_os"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["old_token"], name: "index_sessions_on_old_token", unique: true
     t.index ["token"], name: "index_sessions_on_token", unique: true
   end
@@ -176,14 +175,14 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.bigint "user_id"
     t.bigint "table_id"
     t.string "etag"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "table_object_collections", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "table_object_id"
     t.bigint "collection_id"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
   end
 
   create_table "table_object_prices", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -201,14 +200,14 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
   create_table "table_object_purchases", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "table_object_id"
     t.bigint "purchase_id"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
   end
 
   create_table "table_object_user_accesses", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "table_object_id"
     t.bigint "table_alias"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
   end
 
   create_table "table_objects", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -217,8 +216,8 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "uuid"
     t.boolean "file", default: false
     t.string "etag"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["uuid"], name: "index_table_objects_on_uuid", unique: true
   end
 
@@ -231,8 +230,8 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
   create_table "tables", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "app_id"
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "cdn", default: false
   end
 
@@ -241,12 +240,12 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "ext"
     t.string "mime_type"
     t.string "etag"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_snapshots", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.datetime "time"
+    t.datetime "time", precision: nil
     t.integer "daily_active", default: 0
     t.integer "monthly_active", default: 0
     t.integer "yearly_active", default: 0
@@ -269,13 +268,13 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.string "new_email"
     t.string "new_password"
     t.bigint "used_storage", default: 0
-    t.datetime "last_active"
+    t.datetime "last_active", precision: nil
     t.string "stripe_customer_id"
     t.integer "plan", default: 0
     t.integer "subscription_status", default: 0
-    t.datetime "period_end"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "period_end", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "web_push_subscriptions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -284,7 +283,7 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.text "endpoint"
     t.string "p256dh"
     t.string "auth"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.index ["uuid"], name: "index_web_push_subscriptions_on_uuid", unique: true
   end
 
@@ -292,7 +291,7 @@ ActiveRecord::Schema[6.1].define(version: 2023_10_14_233031) do
     t.bigint "user_id"
     t.bigint "app_id"
     t.string "token", null: false
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
   end
 
 end
