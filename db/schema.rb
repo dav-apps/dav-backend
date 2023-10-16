125ce7a30b00cd9878dc6f81c7f561230adb37e8
David <zurek.dav@gmail.com>
2023-10-16 22:49:49 +0200

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
-  create_table "api_endpoints", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "api_slot_id"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "api_endpoints", force: :cascade do |t|
+    t.bigint "api_slot_id", null: false
     t.string "path"
     t.string "method"
     t.text "commands"
@@ -21,21 +24,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "api_env_vars", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "api_slot_id"
+  create_table "api_env_vars", force: :cascade do |t|
+    t.bigint "api_slot_id", null: false
     t.string "name"
     t.string "value"
     t.string "class_name"
   end
 
-  create_table "api_errors", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "api_slot_id"
+  create_table "api_errors", force: :cascade do |t|
+    t.bigint "api_slot_id", null: false
     t.integer "code"
     t.string "message"
   end
 
-  create_table "api_functions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "api_slot_id"
+  create_table "api_functions", force: :cascade do |t|
+    t.bigint "api_slot_id", null: false
     t.string "name"
     t.string "params"
     t.text "commands"
@@ -43,22 +46,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "api_slots", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "api_id"
+  create_table "api_slots", force: :cascade do |t|
+    t.bigint "api_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "documentation"
+  end
+
+  create_table "apis", force: :cascade do |t|
+    t.bigint "app_id", null: false
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "apis", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "app_id"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "app_user_snapshots", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "app_id"
+  create_table "app_user_snapshots", force: :cascade do |t|
+    t.bigint "app_id", null: false
     t.datetime "time", precision: nil
     t.integer "daily_active", default: 0
     t.integer "monthly_active", default: 0
@@ -71,17 +75,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.integer "email_unconfirmed", default: 0
   end
 
-  create_table "app_users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "app_id"
+  create_table "app_users", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "app_id", null: false
     t.bigint "used_storage", default: 0
     t.datetime "last_active", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "apps", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "dev_id"
+  create_table "apps", force: :cascade do |t|
+    t.bigint "dev_id", null: false
     t.string "name"
     t.string "description"
     t.boolean "published", default: false
@@ -92,22 +96,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "collections", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "table_id"
+  create_table "collections", force: :cascade do |t|
+    t.bigint "table_id", null: false
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "compiled_api_endpoints", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "api_endpoint_id"
+  create_table "compiled_api_endpoints", force: :cascade do |t|
+    t.bigint "api_endpoint_id", null: false
     t.text "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "devs", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "devs", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "api_key"
     t.string "secret_key"
     t.string "uuid"
@@ -115,9 +119,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "notifications", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "app_id"
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "app_id", null: false
     t.string "uuid"
     t.datetime "time", precision: nil
     t.integer "interval"
@@ -128,15 +132,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.index ["uuid"], name: "index_notifications_on_uuid", unique: true
   end
 
-  create_table "providers", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "providers", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "stripe_account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "purchases", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "payment_intent_id"
     t.string "provider_name"
     t.string "provider_image"
@@ -151,16 +155,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.index ["uuid"], name: "index_purchases_on_uuid", unique: true
   end
 
-  create_table "redis_table_object_operations", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+  create_table "redis_table_object_operations", force: :cascade do |t|
     t.string "table_object_uuid"
     t.string "operation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "app_id"
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "app_id", null: false
     t.string "token"
     t.string "old_token"
     t.string "device_name"
@@ -171,48 +175,48 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.index ["token"], name: "index_sessions_on_token", unique: true
   end
 
-  create_table "table_etags", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "table_id"
+  create_table "table_etags", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "table_id", null: false
     t.string "etag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "table_object_collections", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "table_object_id"
-    t.bigint "collection_id"
+  create_table "table_object_collections", force: :cascade do |t|
+    t.bigint "table_object_id", null: false
+    t.bigint "collection_id", null: false
     t.datetime "created_at", null: false
   end
 
-  create_table "table_object_prices", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "table_object_id"
+  create_table "table_object_prices", force: :cascade do |t|
+    t.bigint "table_object_id", null: false
     t.integer "price", default: 0
     t.string "currency", default: "eur"
   end
 
-  create_table "table_object_properties", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "table_object_id"
+  create_table "table_object_properties", force: :cascade do |t|
+    t.bigint "table_object_id", null: false
     t.string "name"
     t.text "value"
   end
 
-  create_table "table_object_purchases", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "table_object_id"
-    t.bigint "purchase_id"
+  create_table "table_object_purchases", force: :cascade do |t|
+    t.bigint "table_object_id", null: false
+    t.bigint "purchase_id", null: false
     t.datetime "created_at", null: false
   end
 
-  create_table "table_object_user_accesses", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "table_object_id"
+  create_table "table_object_user_accesses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "table_object_id", null: false
     t.bigint "table_alias"
     t.datetime "created_at", null: false
   end
 
-  create_table "table_objects", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "table_id"
+  create_table "table_objects", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "table_id", null: false
     t.string "uuid"
     t.boolean "file", default: false
     t.string "etag"
@@ -221,22 +225,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.index ["uuid"], name: "index_table_objects_on_uuid", unique: true
   end
 
-  create_table "table_property_types", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "table_id"
+  create_table "table_property_types", force: :cascade do |t|
+    t.bigint "table_id", null: false
     t.string "name"
     t.integer "data_type", default: 0
   end
 
-  create_table "tables", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "app_id"
+  create_table "tables", force: :cascade do |t|
+    t.bigint "app_id", null: false
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "cdn", default: false
   end
 
-  create_table "user_profile_images", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
+  create_table "user_profile_images", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "ext"
     t.string "mime_type"
     t.string "etag"
@@ -244,7 +248,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_snapshots", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+  create_table "user_snapshots", force: :cascade do |t|
     t.datetime "time", precision: nil
     t.integer "daily_active", default: 0
     t.integer "monthly_active", default: 0
@@ -257,7 +261,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.integer "email_unconfirmed", default: 0
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
     t.string "password_digest"
@@ -277,8 +281,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "web_push_subscriptions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "session_id"
+  create_table "web_push_subscriptions", force: :cascade do |t|
+    t.bigint "session_id", null: false
     t.string "uuid"
     t.text "endpoint"
     t.string "p256dh"
@@ -287,11 +291,49 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_233958) do
     t.index ["uuid"], name: "index_web_push_subscriptions_on_uuid", unique: true
   end
 
-  create_table "websocket_connections", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "app_id"
+  create_table "websocket_connections", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "app_id", null: false
     t.string "token", null: false
     t.datetime "created_at", null: false
   end
 
+  add_foreign_key "api_endpoints", "api_slots", name: "api_endpoints_api_slot_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "api_env_vars", "api_slots", name: "api_env_vars_api_slot_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "api_errors", "api_slots", name: "api_errors_api_slot_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "api_functions", "api_slots", name: "api_functions_api_slot_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "api_slots", "apis", name: "api_slots_api_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "apis", "apps", name: "apis_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "app_user_snapshots", "apps", name: "app_user_snapshots_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "app_users", "apps", name: "app_users_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "app_users", "users", name: "app_users_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "apps", "devs", name: "apps_dev_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "collections", "tables", name: "collections_table_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "compiled_api_endpoints", "api_endpoints", name: "compiled_api_endpoints_api_endpoint_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "devs", "users", name: "devs_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "notifications", "apps", name: "notifications_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "notifications", "users", name: "notifications_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "providers", "users", name: "providers_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "purchases", "users", name: "purchases_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "sessions", "apps", name: "sessions_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "sessions", "users", name: "sessions_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_etags", "tables", name: "table_etags_table_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_etags", "users", name: "table_etags_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_collections", "collections", name: "table_object_collections_collection_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_collections", "table_objects", name: "table_object_collections_table_object_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_prices", "table_objects", name: "table_object_prices_table_object_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_properties", "table_objects", name: "table_object_properties_table_object_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_purchases", "purchases", name: "table_object_purchases_purchase_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_purchases", "table_objects", name: "table_object_purchases_table_object_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_user_accesses", "table_objects", name: "table_object_user_accesses_table_object_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_object_user_accesses", "tables", column: "table_alias", name: "table_object_user_accesses_table_alias_fkey", on_update: :cascade, on_delete: :nullify
+  add_foreign_key "table_object_user_accesses", "users", name: "table_object_user_accesses_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_objects", "tables", name: "table_objects_table_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_objects", "users", name: "table_objects_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "table_property_types", "tables", name: "table_property_types_table_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "tables", "apps", name: "tables_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "user_profile_images", "users", name: "user_profile_images_user_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "web_push_subscriptions", "sessions", name: "web_push_subscriptions_session_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "websocket_connections", "apps", name: "websocket_connections_app_id_fkey", on_update: :cascade, on_delete: :restrict
+  add_foreign_key "websocket_connections", "users", name: "websocket_connections_user_id_fkey", on_update: :cascade, on_delete: :restrict
 end
